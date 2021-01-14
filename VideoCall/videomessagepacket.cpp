70cfcbe56e6f941502614b7c92e19bb23ed1d072
omercankilic <omercankkilic@gmail.com>
2021-01-14 21:28:46 +0300

    decode encode gelisitirildi

---
+++ VideoCall/videomessagepacket.cpp
@@ -0,0 +1,6 @@
+#include "videomessagepacket.h"
+
+VideoMessagePacket::VideoMessagePacket()
+{
+    
+}
---
+++ VideoCall/videomessagepacket.h
@@ -0,0 +1,11 @@
+#ifndef VIDEOMESSAGEPACKET_H
+#define VIDEOMESSAGEPACKET_H
+
+
+class VideoMessagePacket
+{
+    public:
+        VideoMessagePacket();
+};
+
+#endif // VIDEOMESSAGEPACKET_H
